de2e472d86e569e4661bcf786f3b2f1c1794fa0e
Holanda Junior <joaoholandajr@gmail.com>
2015-05-06 17:35:13 -0300

    Package renamed

--- src/br/ufc/datatransfer/clientrequest/IClientRequest.java
+++
@@ -1,9 +0,0 @@
-package br.ufc.datatransfer.clientrequest;
-
-import br.ufc.datatransfer.DataTransferResponse;
-
-
-public interface IClientRequest<T>{
-
-	public DataTransferResponse<T> doConnection();
-}
